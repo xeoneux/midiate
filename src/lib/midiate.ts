6d10f7b80a63b7ce16dc1dbfc75481d85f836e19
Aayush Kapoor <xeoneux@icloud.com>
2018-04-14 00:27:27 +0530

    Fetch all time signature events

--- src/lib/midiate.ts
+++ src/lib/midiate.ts
@@ -1,1 +1,23 @@
-export class Midiate {}
+import { Player, Event } from "midi-player-js";
+
+import { getTimeSignatureEvents } from "../tools/TimeSignature";
+
+const player = new Player();
+
+export class Midiate {
+  ppq: number;
+  tracks: Event[][];
+
+  constructor(location: string) {
+    player.loadFile(location);
+    this.ppq = player.division;
+
+    const tracks: any = player.getEvents();
+    this.tracks = tracks;
+  }
+
+  calculateMeasures = () => {
+    player.getTotalTicks();
+    getTimeSignatureEvents(this.tracks);
+  };
+}
--- src/tools/TimeSignature.ts
+++ src/tools/TimeSignature.ts
@@ -1,3 +1,5 @@
+import { Event } from "midi-player-js";
+
 export interface TimeSignature {
   upper: number;
   lower: number;
@@ -10,6 +12,30 @@ export interface TimeSignatureEvent {
 export function parseTimeSignature(value: string): TimeSignature {
   const index = value.indexOf("/");
   const upper = parseInt(value.slice(0, index));
-  const lower = parseInt(value.slice(index, value.length));
+  const lower = parseInt(value.slice(index + 1, value.length));
   return { upper, lower };
+}
+
+export function getTimeSignatureEvents(
+  tracks: Event[][]
+): TimeSignatureEvent[] {
+  let initialExists = false;
+  let timeSignatureEvents: TimeSignatureEvent[] = [];
+  tracks.forEach(events =>
+    events.forEach(event => {
+      if (event.name === "Time Signature") {
+        const timeSignatureEvent: TimeSignatureEvent = {};
+        timeSignatureEvent[event.tick] = parseTimeSignature(
+          event.timeSignature
+        );
+        timeSignatureEvents.push(timeSignatureEvent);
+        if (event.tick === 0) initialExists = true;
+      }
+    })
+  );
+
+  if (!initialExists)
+    timeSignatureEvents.unshift({ 0: { upper: 4, lower: 4 } });
+
+  return timeSignatureEvents;
 }
