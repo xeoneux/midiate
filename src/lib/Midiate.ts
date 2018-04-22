74f87d31d0d2f4c63f93a081715b4163ec705bae
Aayush Kapoor <xeoneux@icloud.com>
2018-04-22 19:42:49 +0530

    Pre calculate midi data on load

--- src/lib/Midiate.ts
+++ src/lib/Midiate.ts
@@ -1,19 +1,27 @@
-import { Player } from "midi-player-js";
+import { Player } from 'midi-player-js';
 
-import { getMeasures, Measure } from "../tools/Measure";
-import { getTimeSignatures } from "../tools/TimeSignature";
+import { getMeasures, Measure } from '../tools/Measure';
+import { getTimeSignatures } from '../tools/TimeSignature';
 import {
   Note,
   getNoteEventTracks,
   generateNotesForMeasures
-} from "../tools/Notes";
+} from '../tools/Notes';
 
 const player = new Player();
 
 export class Midiate {
+  division: number;
+  measures: Measure[];
+  notes: Note[][];
+
   constructor(data: ArrayBuffer | string) {
-    if (typeof data === "string") player.loadDataUri(data);
-    if (typeof data === "object") player.loadArrayBuffer(data);
+    if (typeof data === 'string') player.loadDataUri(data);
+    if (typeof data === 'object') player.loadArrayBuffer(data);
+
+    this.division = player.division;
+    this.measures = this.calculateMeasures();
+    this.notes = this.calculateNotes(this.measures);
   }
 
   public calculateMeasures = (): Measure[] => {
